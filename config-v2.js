0ba728fcf2ee81c2f816572b9f892ac323e3151b
Rush2007 <144612802+Rush2007@users.noreply.github.com>
2024-12-01 19:12:48 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94753335072"
+const SESSION_ID = "PRABATH-MD~c2d0SIDT#Sv5uouSW0Ekx7CWV2nNvqdvyvxK24qsqbfYoZ4OvhUU"
 
 
 module.exports = {
